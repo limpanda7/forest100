da1fa70d7c5e8cba99cfdfab095a6a224ac90cd8
임성훈 <limpanda7@naver.com>
2022-08-12 11:02:15 +0900

    - 백년한옥 2박이상 예약가능

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 module.exports = global.config = {
-    forest_weekday: 250000,
-    forest_weekend: 300000,
-    forest_holiday: 400000,
+    forest_weekday: 200000,
+    forest_weekend: 250000,
+    forest_holiday: 300000,
     onoff_weekday: 200000,
     onoff_weekend: 300000,
     onoff_holiday: 400000
