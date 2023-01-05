0d6be7aaece737e9eeefb9131b74920029d67d81
임성훈 <sunghoon92@coinone.com>
2023-01-05 16:36:10 +0900

    온오프 신년 할인

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ module.exports = global.config = {
     forest_weekday: 200000,
     forest_weekend: 250000,
     forest_holiday: 300000,
-    onoff_weekday: 200000,
-    onoff_weekend: 300000,
-    onoff_holiday: 400000
+    onoff_weekday: 180000,
+    onoff_weekend: 200000,
+    onoff_holiday: 300000
 };
