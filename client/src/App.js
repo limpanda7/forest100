e8f5cb17ef92bc90ed1c29aa264c1ee1891d7999
임성훈 <limpanda7@naver.com>
2023-01-17 23:15:52 +0900

    React Native 테스트 페이지 제거

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Main from './Main/Main';
 import Forest from './Forest/Forest';
 import OnOff from './OnOff/OnOff';
 import Admin from "./Admin/Admin";
-import RN from "./RN/RN";
 
 const App = () => {
     return (
@@ -13,7 +12,6 @@ const App = () => {
                 <Route exact path='/forest' component={Forest}/>
                 <Route exact path='/on-off' component={OnOff}/>
                 <Route exact path='/admin' component={Admin}/>
-                <Route exact path='/rn' component={RN}/>
                 <Route path='/' component={Main}/>
             </Switch>
         </BrowserRouter>
