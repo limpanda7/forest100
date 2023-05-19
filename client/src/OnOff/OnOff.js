ab3e4f58c30906f0eb45c239583e36785446d219
leafcis <leaf_cis@naver.com>
2023-05-20 02:48:43 +0900

    feat: 에어비앤비와 체크아웃 처리

--- client/src/OnOff/OnOff.js
+++ client/src/OnOff/OnOff.js
@@ -1,131 +1,135 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {Helmet} from "react-helmet";
-import 'react-calendar/dist/Calendar.css';
-import moment from 'moment';
-import './OnOff.scss';
+import { Helmet } from "react-helmet";
+import "react-calendar/dist/Calendar.css";
+import moment from "moment";
+import "./OnOff.scss";
 import OnOffReservation from "./OnOffReservation";
 import OnOffIntro from "./OnOffIntro";
 import OnOffCalendar from "./OnOffCalendar";
-import {Link} from "react-router-dom";
-import cn from 'classnames';
+import { Link } from "react-router-dom";
+import cn from "classnames";
 import OnOffReview from "./OnOffReview";
 
 const OnOff = () => {
-    const [currentPage, setCurrentPage] = useState('intro');
-    const [reserved, setReserved] = useState([]);
-    const [reservedName, setReservedName] = useState([]);
-    const [reservedPhone, setReservedPhone] = useState([]);
-    const [picked, setPicked] = useState([]);
+  const [currentPage, setCurrentPage] = useState("intro");
+  const [reserved, setReserved] = useState([]);
+  const [reservedName, setReservedName] = useState([]);
+  const [reservedPhone, setReservedPhone] = useState([]);
+  const [picked, setPicked] = useState([]);
 
-    const getReserved = () => {
-        axios.get('/api/getReserved2')
-            .then((res) => {
-                let tempReserved = [];
-                let tempReservedName = [];
-                let tempReservedPhone = [];
-                for (const element of res.data) {
-                    tempReserved.push(moment(element.date).format('YYYY-MM-DD'));
+  const getReserved = async () => {
+    const pageReserved = await axios.get("/api/reservation/on_off");
+    const airbnbReserved = await axios.get("/api/ical/on_off");
 
-                    if (!tempReservedName.includes(element.name)) {
-                        tempReservedName.push(element.name);
-                    }
+    let tempReserved = [];
+    let tempReservedName = [];
+    let tempReservedPhone = [];
+    for (const element of pageReserved.data) {
+      tempReserved.push({
+        checkin_date: new Date(element.checkin_date.slice(0, -5)).toString(),
+        checkout_date: new Date(element.checkout_date.slice(0, -5)).toString(),
+      });
 
-                    if (!tempReservedPhone.includes(element.phone)) {
-                        tempReservedPhone.push(element.phone);
-                    }
-                }
-                setReserved(tempReserved);
-                setReservedName(tempReservedName);
-                setReservedPhone(tempReservedPhone);
-            });
+      if (!tempReservedName.includes(element.name)) {
+        tempReservedName.push(element.name);
+      }
+
+      if (!tempReservedPhone.includes(element.phone)) {
+        tempReservedPhone.push(element.phone);
+      }
     }
+    for (const element of airbnbReserved.data) {
+      tempReserved.push({
+        checkin_date: new Date(element.start_dt).toString(),
+        checkout_date: new Date(element.end_dt).toString(),
+      });
 
-    const goToHome = () => {
-        setCurrentPage('calendar');
-        setPicked([]);
+      if (!tempReservedName.includes(element.name)) {
+        tempReservedName.push(element.name);
+      }
+
+      if (!tempReservedPhone.includes(element.phone)) {
+        tempReservedPhone.push(element.phone);
+      }
     }
+    setReserved(tempReserved);
+    setReservedName(tempReservedName);
+    setReservedPhone(tempReservedPhone);
+  };
 
-    return (
-        <div className="OnOff">
-            <Helmet>
-                <title>::: 온오프스테이 :::</title>
-            </Helmet>
+  const goToHome = () => {
+    setCurrentPage("calendar");
+    setPicked([]);
+  };
 
-            <div className='Header'>
-                {
-                    currentPage !== 'reservation' ?
-                        <>
-                            <Link className='BackWrap' to='/'>
-                                ◀
-                            </Link>
-                            <div className='Path'>
-                                ON OFF 스테이
-                            </div>
-                        </>
-                        :
-                        <div className='BackWrap' onClick={() => goToHome()}>
-                            ◀
-                            <span className='Back' >뒤로가기</span>
-                        </div>
-                }
-            </div>
+  return (
+    <div className="OnOff">
+      <Helmet>
+        <title>::: 온오프스테이 :::</title>
+      </Helmet>
 
-            {
-                currentPage !== 'reservation' &&
-                <div className='Tabs'>
-                    <div
-                        className={cn('Tab', {Active: currentPage === 'intro'})}
-                        onClick={() => setCurrentPage('intro')}
-                    >
-                        소개
-                    </div>
-                    <div
-                        className={cn('Tab', {Active: currentPage === 'calendar'})}
-                        onClick={() => setCurrentPage('calendar')}
-                    >
-                        예약하기
-                    </div>
-                    <div
-                        className={cn('Tab', {Active: currentPage === 'review'})}
-                        onClick={() => setCurrentPage('review')}
-                    >
-                        후기
-                    </div>
-                </div>
-            }
+      <div className="Header">
+        {currentPage !== "reservation" ? (
+          <>
+            <Link className="BackWrap" to="/">
+              ◀
+            </Link>
+            <div className="Path">ON OFF 스테이</div>
+          </>
+        ) : (
+          <div className="BackWrap" onClick={() => goToHome()}>
+            ◀<span className="Back">뒤로가기</span>
+          </div>
+        )}
+      </div>
 
-            {
-                currentPage === 'intro' &&
-                <OnOffIntro />
-            }
-            {
-                currentPage === 'calendar' &&
-                <OnOffCalendar
-                    picked={picked}
-                    setPicked={setPicked}
-                    setCurrentPage={setCurrentPage}
-                    getReserved={getReserved}
-                    reserved={reserved}
-                />
-            }
-            {
-                currentPage === 'reservation' &&
-                <OnOffReservation
-                    picked={picked}
-                    setPicked={setPicked}
-                    setCurrentPage={setCurrentPage}
-                    getReserved={getReserved}
-                    reservedName={reservedName}
-                    reservedPhone={reservedPhone}
-                />
-            }
-            {
-                currentPage === 'review' &&
-                <OnOffReview />
-            }
+      {currentPage !== "reservation" && (
+        <div className="Tabs">
+          <div
+            className={cn("Tab", { Active: currentPage === "intro" })}
+            onClick={() => setCurrentPage("intro")}
+          >
+            소개
+          </div>
+          <div
+            className={cn("Tab", { Active: currentPage === "calendar" })}
+            onClick={() => setCurrentPage("calendar")}
+          >
+            예약하기
+          </div>
+          <div
+            className={cn("Tab", { Active: currentPage === "review" })}
+            onClick={() => setCurrentPage("review")}
+          >
+            후기
+          </div>
         </div>
-    );
-}
+      )}
+
+      {currentPage === "intro" && <OnOffIntro />}
+      {currentPage === "calendar" && (
+        <OnOffCalendar
+          picked={picked}
+          setPicked={setPicked}
+          setCurrentPage={setCurrentPage}
+          getReserved={getReserved}
+          reserved={reserved}
+        />
+      )}
+      {currentPage === "reservation" && (
+        <OnOffReservation
+          picked={picked}
+          setPicked={setPicked}
+          setCurrentPage={setCurrentPage}
+          getReserved={getReserved}
+          reservedName={reservedName}
+          reservedPhone={reservedPhone}
+        />
+      )}
+      {currentPage === "review" && <OnOffReview />}
+    </div>
+  );
+};
 
 export default OnOff;
