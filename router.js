10653a52b6494496afe7bef1273dcc614fd81659
임성훈 <limpanda7@naver.com>
2023-07-05 21:28:07 +0900

    Revert "express router wildcard"
    This reverts commit 84556ffb5dff4c4510842fc7f839c5ee2e8ec6f4.

--- router.js
+++ router.js
@@ -7,7 +7,16 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-router.get("/*", (req, res) => {
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+router.get("/forest", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+router.get("/on-off", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+router.get("/boulogne", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
