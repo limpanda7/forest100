e1a69b899e20f0ff1840c2734ec882a3c0b3f5f5
임성훈 <limpanda7@naver.com>
2024-08-19 09:51:25 +0900

    사과 페이지 title 설정

--- client/src/components/Apple/Apple.js
+++ client/src/components/Apple/Apple.js
@@ -3,6 +3,7 @@ import {useNavigate} from "react-router-dom";
 import './Apple.scss';
 import AppleImg from '../../images/Apple.jpg';
 import axios from "axios";
+import {Helmet} from "react-helmet";
 
 const fiveKgPrice = 54000;
 const tenKgPrice = 88000;
@@ -84,105 +85,111 @@ const Apple = () => {
   }
 
   return (
-    <div className='reservation-page'>
-      <img src={AppleImg} alt="AppleImg" />
-      <div className='Apple contents'>
-        <section className='HowMany'>
-          <h2>🍎수량</h2>
-          <div>
-            <p>5 kg</p>
-            <button onClick={() => {
-              if (fiveKg > 0) setFiveKg(fiveKg - 1)
-            }}>-
+    <>
+      <Helmet>
+        <title>::: 백년한옥사과 :::</title>
+      </Helmet>
+
+      <div className='reservation-page'>
+        <img src={AppleImg} alt="AppleImg" />
+        <div className='Apple contents'>
+          <section className='HowMany'>
+            <h2>🍎수량</h2>
+            <div>
+              <p>5 kg</p>
+              <button onClick={() => {
+                if (fiveKg > 0) setFiveKg(fiveKg - 1)
+              }}>-
+              </button>
+              <span>{fiveKg}</span>
+              <button onClick={() => {
+                setFiveKg(fiveKg + 1)
+              }}>+
+              </button>
+            </div>
+            <div>
+              <p>10 kg</p>
+              <button onClick={() => {
+                if (tenKg > 0) setTenKg(tenKg - 1)
+              }}>-
+              </button>
+              <span>{tenKg}</span>
+              <button onClick={() => {
+                setTenKg(tenKg + 1)
+              }}>+
+              </button>
+            </div>
+          </section>
+
+          <section className='PriceTotal'>
+            <h2>🍎총 금액</h2>
+            <h2 className='Price'>{price.toLocaleString()}원</h2>
+            <div className='PriceDetail'>
+              <p><b>5 kg:</b> {fiveKgPrice.toLocaleString()} x {fiveKg}박스 = {(fiveKgPrice * fiveKg).toLocaleString()}</p>
+              <p><b>10 kg:</b> {tenKgPrice.toLocaleString()} x {tenKg}박스 = {(tenKgPrice * tenKg).toLocaleString()}</p>
+            </div>
+          </section>
+
+          <section className='receiver-section'>
+            <h2>🍎받으실 분</h2>
+            <div>
+              <span className='input-label'>받으실 분 성함:</span>
+              <input type='text' value={receiverName} size='6'
+                     onChange={(e) => setReceiverName(e.target.value)}/>
+            </div>
+
+            <div>
+              <span className='input-label'>받으실 분 전화번호:</span>
+              <input type='text' value={receiverPhone} size='14'
+                     onChange={(e) => setReceiverPhone(e.target.value)}/>
+            </div>
+
+            <div>
+              <span className='input-label'>받으실 분 주소:</span>
+              <input type='text' value={address} style={{width: '340px'}}
+                     onChange={(e) => setAddress(e.target.value)}/>
+            </div>
+          </section>
+
+          <section className='Deposit'>
+            <h2>🍎입금하기</h2>
+            <div className='BankAccount'>카카오 79420205681 남은비</div>
+            <p>
+              위 계좌로 <b>{price.toLocaleString()}원</b>을 입금해주세요.<br/>
+            </p>
+            <p>
+              <span className='input-label'>입금하실 분 성함:</span>
+              <input
+                type='text'
+                size='6'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                disabled={isSameAsReceiver}
+              /><br/>
+              <span className='input-label'>전화번호:</span>
+              <input
+                type='text'
+                size='14'
+                pattern='[0-9]*'
+                value={phone}
+                maxLength={11}
+                onChange={(e) => {
+                  if (e.target.validity.valid) setPhone(e.target.value)
+                }}
+                disabled={isSameAsReceiver}
+              /><br/>
+              <label className="checkbox-wrap">
+                <input type="checkbox" checked={isSameAsReceiver} onChange={handleCheckboxChange}/>
+                <span>받는 사람과 동일</span>
+              </label>
+            </p>
+            <button className='large-btn' onClick={saveReservation}>
+              예약하기
             </button>
-            <span>{fiveKg}</span>
-            <button onClick={() => {
-              setFiveKg(fiveKg + 1)
-            }}>+
-            </button>
-          </div>
-          <div>
-            <p>10 kg</p>
-            <button onClick={() => {
-              if (tenKg > 0) setTenKg(tenKg - 1)
-            }}>-
-            </button>
-            <span>{tenKg}</span>
-            <button onClick={() => {
-              setTenKg(tenKg + 1)
-            }}>+
-            </button>
-          </div>
-        </section>
-
-        <section className='PriceTotal'>
-          <h2>🍎총 금액</h2>
-          <h2 className='Price'>{price.toLocaleString()}원</h2>
-          <div className='PriceDetail'>
-            <p><b>5 kg:</b> {fiveKgPrice.toLocaleString()} x {fiveKg}박스 = {(fiveKgPrice * fiveKg).toLocaleString()}</p>
-            <p><b>10 kg:</b> {tenKgPrice.toLocaleString()} x {tenKg}박스 = {(tenKgPrice * tenKg).toLocaleString()}</p>
-          </div>
-        </section>
-
-        <section className='receiver-section'>
-          <h2>🍎받으실 분</h2>
-          <div>
-            <span className='input-label'>받으실 분 성함:</span>
-            <input type='text' value={receiverName} size='6'
-                   onChange={(e) => setReceiverName(e.target.value)}/>
-          </div>
-
-          <div>
-            <span className='input-label'>받으실 분 전화번호:</span>
-            <input type='text' value={receiverPhone} size='14'
-                   onChange={(e) => setReceiverPhone(e.target.value)}/>
-          </div>
-
-          <div>
-            <span className='input-label'>받으실 분 주소:</span>
-            <input type='text' value={address} style={{width: '340px'}}
-                   onChange={(e) => setAddress(e.target.value)}/>
-          </div>
-        </section>
-
-        <section className='Deposit'>
-          <h2>🍎입금하기</h2>
-          <div className='BankAccount'>카카오 79420205681 남은비</div>
-          <p>
-            위 계좌로 <b>{price.toLocaleString()}원</b>을 입금해주세요.<br/>
-          </p>
-          <p>
-            <span className='input-label'>입금하실 분 성함:</span>
-            <input
-              type='text'
-              size='6'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              disabled={isSameAsReceiver}
-            /><br/>
-            <span className='input-label'>전화번호:</span>
-            <input
-              type='text'
-              size='14'
-              pattern='[0-9]*'
-              value={phone}
-              maxLength={11}
-              onChange={(e) => {
-                if (e.target.validity.valid) setPhone(e.target.value)
-              }}
-              disabled={isSameAsReceiver}
-            /><br/>
-            <label className="checkbox-wrap">
-              <input type="checkbox" checked={isSameAsReceiver} onChange={handleCheckboxChange}/>
-              <span>받는 사람과 동일</span>
-            </label>
-          </p>
-          <button className='large-btn' onClick={saveReservation}>
-            예약하기
-          </button>
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
