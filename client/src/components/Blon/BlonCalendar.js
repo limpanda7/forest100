5fc62307888baf42c851134aa533374707412e84
Sunghoon <limpanda7@naver.com>
2024-11-06 21:26:18 +0900

    (긴급) 예약 시스템 점검
    (cherry picked from commit 289616256b33a1adb01d6c05cc26c550962e14ae)

--- client/src/components/Blon/BlonCalendar.js
+++ client/src/components/Blon/BlonCalendar.js
@@ -139,21 +139,29 @@ const BlonCalendar = ({isLoading, picked, setPicked, setCurrentPage, reserved}) 
           (체크인 3시 / 체크아웃 11시)
         </p>
 
-        {
-          isLoading ?
-            <div className='calendar'>
-              <div className='loading'>
-                <div className='spinner'/>
-              </div>
-            </div>
-            :
-            <Calendar
-              isContinuous={true}
-              picked={picked}
-              setPicked={setPicked}
-              reserved={reserved}
-            />
-        }
+        <p>
+          현재 예약시스템 점검중입니다.<br/>
+          아래 연락처를 통해 예약 도와드리겠습니다<br/>
+          <a href='https://www.instagram.com/boulogne_forest/' target='_blank' className='anchor'>
+            <p>@boulogne_forest</p>
+          </a>
+        </p>
+
+        {/*{*/}
+        {/*  isLoading ?*/}
+        {/*    <div className='calendar'>*/}
+        {/*      <div className='loading'>*/}
+        {/*        <div className='spinner'/>*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*    :*/}
+        {/*    <Calendar*/}
+        {/*      isContinuous={true}*/}
+        {/*      picked={picked}*/}
+        {/*      setPicked={setPicked}*/}
+        {/*      reserved={reserved}*/}
+        {/*    />*/}
+        {/*}*/}
         <button className="large-btn" onClick={moveToReservation}>
           선택한 날짜로 예약하기
         </button>
