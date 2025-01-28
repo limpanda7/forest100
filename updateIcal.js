92b401f780d9ffe67cb1a67258cc255a7d1f4ab7
Sunghoon <limpanda7@naver.com>
2025-01-28 18:48:41 +0900

    백엔드 ical 에러 시 텔레그램 알림

--- updateIcal.js
+++ updateIcal.js
@@ -2,6 +2,7 @@ import ical from "node-ical";
 import moment from "moment";
 import mysql from "mysql";
 import 'dotenv/config';
+import TelegramBot from "node-telegram-bot-api";
 
 // 데이터베이스 연결
 const connection = mysql.createConnection({
@@ -13,7 +14,19 @@ const connection = mysql.createConnection({
 })
 connection.connect();
 
+// 텔레그램 봇
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token);
+
 export const updateIcal = (url, target) => {
+
+  let chatId = null;
+  if (target === 'forest') {
+    chatId = process.env.TELEGRAM_CHAT_ID_FOREST;
+  } else if (target === 'blon') {
+    chatId = process.env.TELEGRAM_CHAT_ID_BLON;
+  }
+
   try {
     ical.fromURL(
       url,
@@ -21,11 +34,13 @@ export const updateIcal = (url, target) => {
       (err, res) => {
         if (err) {
           console.error(`Failed to fetch iCal data: ${err}`);
+          bot.sendMessage(chatId, `iCal 데이터 가져오기 실패: ${err.message}`);
           return;
         }
 
         if (!res) {
           console.error("Error: iCal response is undefined or null.");
+          bot.sendMessage(chatId, `iCal 응답이 비어있습니다. 대상: ${target}`);
           return;
         }
 
@@ -57,12 +72,14 @@ export const updateIcal = (url, target) => {
 
         if (values.length === 0) {
           console.log(`No data to insert for target: ${target}_ical`);
+          bot.sendMessage(chatId, `iCal 데이터가 비어 있습니다 대상: ${target}`);
           return;
         }
 
         connection.query(`TRUNCATE TABLE ${target}_ical`, (truncateErr) => {
           if (truncateErr) {
             console.error(`Failed to truncate table: ${truncateErr}`);
+            bot.sendMessage(chatId, `테이블 정리 실패: ${truncateErr.message}`);
             return;
           }
 
@@ -72,6 +89,7 @@ export const updateIcal = (url, target) => {
             (insertErr) => {
               if (insertErr) {
                 console.error(`Failed to insert data into ${target}_ical: ${insertErr}`);
+                bot.sendMessage(chatId, `데이터 삽입 실패: ${insertErr.message}`);
               } else {
                 console.log(`Successfully updated ${target}_ical with ${values.length} records.`);
               }
